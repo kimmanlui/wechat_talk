ef1e2b20e8a085db22b63e55901f1fa526f5ad6b
kimmanlui <cskmlui@hotmail.com>
2017-10-19 19:23:06 +0800

    Add files via upload

--- wintalk.py
+++ wintalk.py
@@ -2,14 +2,29 @@
 # -*- coding: UTF-8 -*-
 # The program is based on wxpy library. 
 # Ref: https://github.com/youfou/wxpy
-
-from wxpy import *
-import datetime
-import codecs
 # FOR PYTHON 27 ON WINDOWS
+# 用户自行设计关键字对话
+#
+
+d={"help"         : "谢谢你对UIC-ACE课程感兴趣，你可以打如下关键字 [1] course  [2] registration [3] discount" , 
+   "test"         : "ACE chatbot is working fine. " ,
+   "course"       : "目前有 [1] 危机管理  [2] SAT 等",
+   "registration" : "使用手机或电脑打开uic.edu.hk/en/ace/application然后填下表格，便完成！",
+   "discount"     : "some coure has early bird discount" ,
+   "危机管理"      : "这是非常好的课程", 
+   "SAT"          : "early-bird discount before 1 Nov 2017", 
+   "1"            : "不要打数字，打小写英文或中文关键字"  ,
+   "2"            : "不要打数字，打小写英文或中文关键字"  ,
+   "3"            : "不要打数字，打小写英文或中文关键字"  ,
+   "4"            : "不要打数字，打小写英文或中文关键字"  } 
+
 
 #Change the default encoding for Chinese characters
 #http://blog.csdn.net/a657941877/article/details/9063883
+from wxpy import *
+import datetime
+import codecs
+
 import sys   
 reload(sys)   
 sys.setdefaultencoding('utf-8')   
@@ -21,6 +36,13 @@ def get_curtime():
     curtime=datetime.datetime.now().strftime("%I:%M%p on %B %d,%Y")
     return(curtime)
 
+def log_messages(msg):
+    print("log_messages called") #debug purpose
+    file = open(logFile,"a")
+    file.write(msg)
+    file.write(" "+get_curtime()+"\n")
+    file.close() 
+
 def load_conversation():
     d = {}
     with codecs.open(actionFile,'r','GBK') as f:
@@ -29,43 +51,33 @@ def load_conversation():
             d[str(key)] = val
     return(d)
 
-def log_messages(msg):
-    print("log_messages called") #debug purpose
-    file = open(logFile,"a")
-    file.write(msg)
-    file.write(" "+get_curtime()+"\n")
-    file.close() 
-
 
 bot = Bot()
-
 myself = bot.self
-
 startupMessage='微信机械人启动于'+get_curtime()
 bot.file_helper.send(startupMessage)
 
 @bot.register()
 def print_messages(msg):
     d=load_conversation()
-
     print (msg)
     amsg=msg.text
     awho=msg.sender
-    print (awho)
-    #awho.decode('utf-8').encode('gb18030') 
-    #amsg=str(unicode(msg.text()))
-    print(amsg)
-    print(type(awho))
-    print(type(amsg))
+    #print (awho)
+    #print(amsg)
+    #print(type(awho))
+    #print(type(amsg))
     log_messages(amsg)
-    #amsg.decode('ascii')
-    #if amsg == 'test':
-    #  msg.reply("搞定")
-    if amsg in d : 
-      print("triggered")
-      sentMsg=d[amsg]
-      sentMsg.decode('utf-8').encode('gb18030') 
-      msg.reply(sentMsg)
+
+    key=list(d.keys())
+    value=list(d.values())
+    for i in range(len(key)):
+        if (amsg==key[i]):
+            print("triggered")
+            sentMsg=value[i]
+            sentMsg.decode('utf-8').encode('gb18030') 
+            msg.reply(sentMsg)
+        
 
 # The following code is not yet tested
 @bot.register(msg_types=FRIENDS)
